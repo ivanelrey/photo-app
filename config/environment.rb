54de034ba8ee6c6cf635d577122991ec7149565d
ivanelrey <ivanejg@yahoo.com>
2017-06-21 17:27:48 +0300

    setup emailconfirmation for sign up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => :plain,
+	:user_name => 'apikey',
+	:password => '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',
+	:domain => 'heroku.com',
+	:enable_starttls_auto => true
+}

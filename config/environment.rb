e5bd96fa4265486b4ea65ada3d9a9dae1d4d3f33
ivanelrey <ivanejg@yahoo.com>
2017-06-21 17:52:33 +0300

    fix some bugs

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:user_name => 'apikey',
-	:password => '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',
+	:user_name => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
 	:domain => 'heroku.com',
 	:enable_starttls_auto => true
 }
